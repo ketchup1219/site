5cb24eaabd38d6c0704fba2b10e4afc52ba8e21f
ketchup1219 <tomatoketchup1219@gmail.com>
2026-03-19 20:41:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function login() {
+\function login() {
   const id = document.getElementById("id").value;
   const pass = document.getElementById("pass").value;
 
-  // 管理人が設定したIDとパスワード
   const correctId = "ayauta";
   const correctPass = "2023";
 
   if (id === correctId && pass === correctPass) {
     sessionStorage.setItem("loggedIn", "true");
-    window.location.href = "home.html"; // ログイン後のページ
+    window.location.href = "home.html";
   } else {
     alert("IDとパスワードが違うんじゃないんですかね笑");
   }
