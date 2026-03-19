cf6f1af624196de78a01cdb44ceec6c4c0fca193
ketchup1219 <tomatoketchup1219@gmail.com>
2026-03-19 21:36:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function login() {
   const correctPass = "2023";
 
   if (id === correctId && pass === correctPass) {
-    alert("ログイン成功、おかえり");
+    window.location.href = "home.html";
   } else {
     alert("IDとパスワードが違うんじゃないんですかね笑");
   }
