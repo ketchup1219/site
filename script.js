b9a60e86b30c593b3e83b529b0d1b8c1746f0313
ketchup1219 <tomatoketchup1219@gmail.com>
2026-03-19 20:50:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function login() {
-  const id = document.getElementById("id").value;
-  const pass = document.getElementById("pass").value;
-
-  const correctId = "ayauta";
-  const correctPass = "2023";
-
-  if (id === correctId && pass === correctPass) {
-    alert("ログイン成功！");
-  } else {
-    alert("IDとパスワードが違うんじゃないんですかね笑");
-  }
-}
+alert("script.js 読み込まれてるよ！");
